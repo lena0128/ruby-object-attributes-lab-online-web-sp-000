35e6f62d38d5daedf3fc23cc9b123397c9f93e7c
Lena Hou <lenahou2013@gmail.com>
2021-02-06 23:58:01 +0000

    Automatically backed up by Learn

---
+++ lib/dog.rb
@@ -0,0 +1,17 @@
+class Dog
+  def name=(dogs_name)
+    @this_name = dogs_name
+  end
+
+  def name
+    @this_name
+  end
+
+  def breed=(dogs_breed)
+    @this_breed = dogs_breed
+  end
+
+  def breed
+    @this_breed
+  end
+end  
